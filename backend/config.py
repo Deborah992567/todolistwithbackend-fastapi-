b694d9d049df381d103c14b303e6e3f84623295e
uchechiiwuh <debbieofficial214@gmail.com>
2025-09-08 15:32:40 +0200

    changed the database  connection

--- backend/config.py
+++ backend/config.py
@@ -7,4 +7,4 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_MIN", "60"))
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_DAYS", "7"))
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Echolacc11@localhost:5432/TODOLIST")
+DATABASE_URL = os.getenv("DATABASE_URL", 'postgresql://neondb_owner:npg_FE5nCNQay1mg@ep-young-dew-adqfc9xa-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
